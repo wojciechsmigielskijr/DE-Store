0e097213abbd05cc007da7c2e088eb8ec42aa521
wojciechsmigielskijr <43293521+wojciechsmigielskijr@users.noreply.github.com>
2022-08-15 23:20:04 +0100

    Formatting fixes, CSS changes for alert

--- De-Store/De_Store.Service.Inventory/Program.cs
+++ De-Store/De_Store.Service.Inventory/Program.cs
@@ -13,10 +13,7 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader();
     });
 });
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
-// Add services to the container.
 builder.Services.AddGrpc();
 
 var app = builder.Build();
@@ -24,7 +21,7 @@ var app = builder.Build();
 app.UseGrpcWeb();
 
 app.UseCors("DevCorsPolicy");
-// Configure the HTTP request pipeline.
+
 app.MapGrpcService<InventoryService>().EnableGrpcWeb();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
