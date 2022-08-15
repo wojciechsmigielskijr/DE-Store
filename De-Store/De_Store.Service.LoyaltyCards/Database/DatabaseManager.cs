02fd6251930b8c814742b7df557fab943ad02661
wojciechsmigielskijr <43293521+wojciechsmigielskijr@users.noreply.github.com>
2022-08-16 00:39:56 +0100

    Initial loyalty card changes and database.

---
+++ De-Store/De_Store.Service.LoyaltyCards/Database/DatabaseManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace De_Store.Service.LoyaltyCards.Database
+{
+    public class DatabaseManager
+    {
+        public DatabaseManager()
+        {
+            Initialise();
+        }
+        public string? DatabaseConnectionString { get; set; }
+
+        public SqlConnection? SQLConnection { get; set; }
+
+        public void Initialise()
+        {
+            DatabaseConnectionString = "Data Source=WOJCIECHSDESK\\WOJCIECHSDESK;Initial Catalog=De-Store;Integrated Security=True";
+            SQLConnection = new SqlConnection(DatabaseConnectionString);
+        }
+
+        public void CleanUp()
+        {
+            SQLConnection.Close();
+        }
+    }
+}
